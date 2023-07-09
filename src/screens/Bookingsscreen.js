091a53bab3c01a6e5496073866cb04f7be77c5b6
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-07-09 23:11:42 +0530

    booking one

--- src/screens/Bookingsscreen.js
+++ src/screens/Bookingsscreen.js
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 function Bookingsscreen() {
   const { roomid } = useParams();
   const [room, setRoom] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -26,9 +26,59 @@ function Bookingsscreen() {
   }, [roomid]);
 
   return (
-    <div>
-      <h1>Booking screen</h1>
-      <h1>Room id: {roomid}</h1>
+    <div  className='m-5'>
+       {loading ? (<h1>Loading .....</h1>):error ? (<h1>Error</h1>):(<div>
+             <div className="row justify-content-center mt-5 bs">
+
+              <div className='col-md-5'>
+                <h1>{room.name}</h1>
+                <img
+          className="d-block w-100 bigimg"
+          src={room.imageUrls[0] }
+        />
+
+              </div>
+              
+              <div className='col-md-5'>
+              
+              <div style={{ textAlign:'right' }}>
+              <h1>Booking details</h1>
+                <hr></hr>
+                <b>
+                <p>Name:</p>
+                <p>From Date:</p>
+                <p>To Date:</p>
+                <p>Max count:</p>
+                </b>
+
+                <h1>Amount</h1>
+                <hr></hr>
+                <b>
+                <p>Total days:</p>
+                <p>Rent Per day:{room.rentperday}</p>
+                <p>Total Amount:</p>
+                </b>
+
+              </div>
+              
+            
+
+                <div style={{ float:'right' }}>
+                  <button className='btn btn-primary'>Pay Now</button>
+                </div>
+
+
+                <div>
+                  
+
+                </div>
+              </div>
+
+
+             </div>
+
+
+       </div>)}
     </div>
   );
 }
