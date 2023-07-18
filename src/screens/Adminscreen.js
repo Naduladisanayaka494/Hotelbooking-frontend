dd80ecf19a985ec4bdeea4a78b6064950a94ea2d
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-07-18 21:25:37 +0530

    Admin panel design

--- src/screens/Adminscreen.js
+++ src/screens/Adminscreen.js
@@ -8,6 +8,16 @@ const onChange = (key: string) => {
 };
 
 function Adminscreen() {
+  const user = JSON.parse(localStorage.getItem('currentUser'));
+
+  useEffect(() => {
+    if (!JSON.parse(localStorage.getItem('currentUser')).data.isAdmin) {
+      window.location.href = '/home';
+    }
+  }, [])
+
+
+
   const items: TabsProps['items'] = [
     {
       key: '1',
@@ -19,7 +29,7 @@ function Adminscreen() {
     {
       key: '2',
       label: 'Rooms',
-      children: <h1>Rooms</h1>,
+      children: <Rooms/>,
     },
     {
       key: '3',
@@ -29,7 +39,7 @@ function Adminscreen() {
     {
       key: '4',
       label: 'Users',
-      children: <h1>Add Users</h1>,
+      children:<Users/>,
     },
   ];
 
@@ -70,11 +80,147 @@ export function Bookings() {
         <div className='col-md-10'>
           <h1>Bookings</h1>
           {loading && <Loader />}
-          {bookings.length > 0 && <h1>There are a total of {bookings.length} bookings</h1>}
+          <table className='table table-bordered table-dark' style={{ backgroundColor: 'black' }}>
+            <thead className='bs'>
+              <tr>
+                <th>Booking Id</th>
+                <th>User Id</th>
+                <th>Room</th>
+                <th>From</th>
+                <th>To</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookings.length > 0 && bookings.map(booking => (
+                <tr key={booking._id}>
+                  <td>{booking._id}</td>
+                  <td>{booking.userid}</td>
+                  <td>{booking.room}</td>
+                  <td>{booking.fromdate}</td>
+                  <td>{booking.todate}</td>
+                  <td>{booking.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
           {error && <p>An error occurred while fetching bookings.</p>}
         </div>
       </div>
     );
+    
   }
+
+  export function Rooms() {
+    const [rooms, setRooms] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('/api/rooms/getallrooms');
+          setRooms(response.data);
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
+          setError(true);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    return (
+      <div className='row'>
+        <div className='col-md-10'>
+          <h1>Rooms</h1>
+          {loading && <Loader />}
+          <table className='table table-bordered table-dark' style={{ backgroundColor: 'black' }}>
+            <thead className='bs'>
+              <tr>
+                <th>Room Id</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Rent Per Day</th>
+                <th>Max Count</th>
+                <th>Phonenumber</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rooms.length > 0 && rooms.map(booking => (
+                <tr key={booking._id}>
+                  <td>{booking._id}</td>
+                  <td>{booking.name}</td>
+                  <td>{booking.type}</td>
+                  <td>{booking.rentperday}</td>
+                  <td>{booking.maxcount}</td>
+                  <td>{booking.phonenumber}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {error && <p>An error occurred while fetching bookings.</p>}
+        </div>
+      </div>
+    );
+    
+  }
+
+
+  export function Users() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('/api/users/getallusers');
+          setUsers(response.data);
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
+          setError(true);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    return (
+      <div className='row'>
+        <div className='col-md-10'>
+          <h1>Users</h1>
+          {loading && <Loader />}
+          <table className='table table-bordered table-dark' style={{ backgroundColor: 'black' }}>
+            <thead className='bs'>
+              <tr>
+                <th>User Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Is Admin</th>
+            
+              </tr>
+            </thead>
+            <tbody>
+              {users.length > 0 && users.map(booking => (
+                <tr key={booking._id}>
+                  <td>{booking._id}</td>
+                  <td>{booking.name}</td>
+                  <td>{booking.email}</td>
+                  <td>{booking.isAdmin  ? 'YES':'NO'}</td>
+                 
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {error && <p>An error occurred while fetching bookings.</p>}
+        </div>
+      </div>
+    );
+    
+  }
+
   
   
